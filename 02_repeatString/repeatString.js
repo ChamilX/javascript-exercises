80ea9e346ab8a2a23c384099031793018f25f741
ChamilX <chamilxavier@gmail.com>
2024-05-14 03:35:25 -0400

    Completed exercises 01 to 07
    Finished exercises from 01_helloWorld to 07_tempConversion.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    let repeatedString = "";
+    if (number >= 0){
+        for (i = 0; i < number; i++) {
+            repeatedString += word;
+        }
+        return repeatedString;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toRemove) {
+    return array.filter(number => !toRemove.includes(number));
 };
 
 // Do not edit below this line
