efbe20b29ea70b11b02827bcabac4b01cc77b417
ChamilX <chamilxavier@gmail.com>
2024-07-07 02:26:06 -0400

    Complete exercise 09
    Completed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let wordsArray = words.split("").reverse().join("").toLowerCase();
+    if ( wordsArray.replace(/[^A-Za-z1-9]/g, "") === words.replace(/[^A-Za-z1-9]/g, "").toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
