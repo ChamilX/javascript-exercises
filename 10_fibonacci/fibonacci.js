9999ff907d4e0cb387eddb1f564c7619473534fa
ChamilX <chamilxavier@gmail.com>
2024-08-01 02:14:03 -0400

    Complete exercise 10
    Completed exercise 10-Fibonacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let total = 0;
+    let prevNum = 0;
+    let nextNum = 1;
+    if (Number(number) < 0){ return "OOPS";}
+    if (Number(number) === 1){return 1;}
+    for (let i = 1; i < Number(number); i++){
+        total = prevNum + nextNum;
+        prevNum = nextNum;
+        nextNum = total;
+    }
+    return total;
 };
 
 // Do not edit below this line
